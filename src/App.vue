151fd605ecdcd61a6932f7839674fe571357343c
Noah Ratcliff <nhrtclff@gmail.com>
2021-10-22 09:38:14 -0400

    style: correct tabs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <bones-status />
-  </div>
+	<div id="app">
+		<bones-status />
+	</div>
 </template>
 
 <script>
 import BonesStatus from './components/BonesStatus.vue'
 
 export default {
-  name: 'Is today a bones day?',
-  components: {
-    BonesStatus
-  }
+	name: 'Is today a bones day?',
+	components: {
+		BonesStatus
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
